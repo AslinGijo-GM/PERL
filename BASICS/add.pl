157077c7c95c371680cb409d0f78ab9bb57cfd2c
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-06-10 19:03:10 +0530

    Create add.pl

---
+++ BASICS/add.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $name = "Perl";
+print "Lang : $name\n";
+
+my $a = 23;  # Added 'my'
+my $b = 56;  # Added 'my'
+
+my $sum = $a + $b; 
+print "sum : $sum\n ";
