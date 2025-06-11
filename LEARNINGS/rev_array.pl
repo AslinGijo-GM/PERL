0ebde2c295e28f32216ee95b4a91ff51ee9a4b20
Aslin Gijo <90519507+AslinGijo-GM@users.noreply.github.com>
2025-06-11 09:33:43 +0530

    Create rev_array.pl

---
+++ LEARNINGS/rev_array.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @fruit = ("apple" , "banana" , "cherry" );
+my @num = ( 10,12,45,56 );
+my @arr = qw(apple red banana yellow kiwi green grape violet);
+
+
+my @first = reverse @fruit;
+
+print "@first\n";
